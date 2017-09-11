fd221e9231cde8305993f614a8168309fa1b0980
Ryan Baldoza <ryanbaldoza@outlook.com>
2017-09-12 07:17:49 +0800

    Added folders to scss and hero banner for home page

--- resources/views/frontend/pages/homepage.blade.php
+++ resources/views/frontend/pages/homepage.blade.php
@@ -1,5 +1,39 @@
 @extends( 'frontend/index' )
 
 @section( 'content' )
-	testhomepage
+
+{{-- HERO BANNER --}}
+<div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="4000">
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+        	<img src="{{ asset('frontend/imgs/slide1.png') }}" alt="Chania">
+        </div>
+        <div class="item">
+        	<img src="{{ asset('frontend/imgs/slide2.png') }}" alt="Chania">
+        </div>
+        <div class="item">
+        	<img src="{{ asset('frontend/imgs/slide3.png') }}" alt="Flower">
+        </div>
+        <div class="slide-content">
+    		<div class="banner-logo"><img src="{{ asset('frontend/imgs/heegus-tagline.png') }}" alt="Heegus"></div>
+			  <form class="form-inline" action="/action_page.php">
+			    <div class="form-group">
+			      <input type="text" class="form-control" placeholder="Search Services"  name="services">
+			    </div>
+			    <div class="form-group">
+			      <input type="text" class="form-control" placeholder="Location" name="location">
+			    </div>
+			    <button type="submit" class="search__btn primary__btn __transition">Search</button>
+			  </form>
+    	</div>
+    </div>
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="sr-only">Next</span>
+    </a>
+</div>
+
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -16,4 +16,5 @@ mix.styles([
 	'node_modules/bootstrap/dist/css/bootstrap.min.css'
 ], common + '/css/vendors.css')
 .js('resources/assets/common/js/vendors.js', common + '/js/vendors.js')
-.sass('resources/assets/common/scss/header.scss', common + '/css');
+.js('resources/assets/common/js/main.js', common + '/js/main.js')
+.sass('resources/assets/common/scss/main.scss', common + '/css');
